2b56d4353fad882747d10ff10219cefb5d7c5f5b
Yun Kui Pan <panyunkui@gmail.com>
2015-11-08 11:10:53 -0500

    organize structure, added: playAgainButton

--- GreedyBird/Scripts/states/game.js
+++ GreedyBird/Scripts/states/game.js
@@ -18,7 +18,6 @@ var states;
         Game.prototype.start = function () {
             // variables
             this._oldTime = createjs.Ticker.getTicks();
-            console.log("Old: " + this._oldTime);
             // add background
             this._background = new objects.Background();
             this.addChild(this._background);
@@ -38,7 +37,6 @@ var states;
             this._healthLabel.regX = 0;
             this._healthLabel.regY = 0;
             this.addChild(this._healthLabel);
-            this._gameOverLabel = new objects.Label("Game Over", "60px Consolas", "#0ff", 320, 240);
             // add bird
             this._bird = new objects.Bird();
             this.addChild(this._bird);
@@ -46,21 +44,48 @@ var states;
                 this._dragons[index] = new objects.Dragon();
                 this.addChild(this._dragons[index]);
             }
+            // When Game Over +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            this._gameOverLabel = new objects.Label("Game Over", "60px Consolas", "#0ff", 320, 140);
+            // playAgainButton
+            this._playAgainButton = new objects.Button("playAgainButton", 320, 240);
+            this._playAgainButton.on("click", this._clickPlayAgainButton, this);
+            this._playAgainButton.visible = false;
+            this.addChild(this._playAgainButton);
             stage.addChild(this);
         }; // end of start()
+        //
+        Game.prototype._clickPlayAgainButton = function (event) {
+            this._resetGame();
+        };
+        Game.prototype._resetGame = function () {
+            // reset
+            this._health = 100;
+            this._isGameOver = false;
+            this._gameOverLabel.visible = false;
+            this._healthLabel.text = this._health.toString();
+            this._healthBar.graphics.beginFill("#0f5").drawRect(50, 15, this._health, 4);
+            this._bird.visible = true;
+            this._bird.y = 150;
+            this._playAgainButton.visible = false;
+        };
         Game.prototype.update = function () {
             this._bird.update();
             this._background.update();
+            this._updateDragons_CheckCollision();
+            this._updateFeathers_RemoveFeathers();
+        }; // end of update
+        Game.prototype._updateDragons_CheckCollision = function () {
+            // because simply reset the positions of dragons, number of dragons never change
             for (var index = 0; index < 3; index++) {
                 this._dragons[index].update();
                 if (!this._isGameOver) {
                     this._checkDistanceBetween(this._bird, this._dragons[index]);
                 }
             }
-            //
-            // update & remove feathers
+        };
+        Game.prototype._updateFeathers_RemoveFeathers = function () {
             if (this._redFeathers.length > 0) {
-                console.log("Feathers: " + this._redFeathers.length);
+                //console.log("Feathers: " + this._redFeathers.length);
                 for (var i = 0, len = this._redFeathers.length; i < len; i++) {
                     this._redFeathers[i].update();
                     if (this._redFeathers[i].y > 680) {
@@ -70,36 +95,43 @@ var states;
                     }
                 }
             }
-        }; // end of update
+        };
+        Game.prototype._updateBirdHealth = function () {
+            this._healthLabel.text = this._health > 0 ? this._health.toString() : "0";
+            this._healthBar.graphics.clear();
+            if (this._health >= 70) {
+                this._healthBar.graphics.beginFill("#0f5").drawRect(50, 15, this._health, 4);
+            }
+            else if (this._health >= 40 && this._health < 70) {
+                this._healthBar.graphics.beginFill("#ff0").drawRect(50, 15, this._health, 4);
+            }
+            else {
+                this._healthBar.graphics.beginFill("#f00").drawRect(50, 15, this._health, 4);
+            }
+        };
+        Game.prototype._setGameOver = function () {
+            this._bird.visible = false;
+            this.addChild(this._gameOverLabel);
+            this._isGameOver = true;
+            this._playAgainButton.visible = true;
+        };
+        Game.prototype._showHurtEffect = function () {
+            // add once feather droping out but not too much
+            this._nowTime = createjs.Ticker.getTicks();
+            if (this._nowTime - this._oldTime >= 30) {
+                this._redFeathers.push(new objects.Feather(this._bird.x, this._bird.y));
+                this.addChild(this._redFeathers[this._redFeathers.length - 1]);
+                this._oldTime = this._nowTime;
+            }
+        };
         Game.prototype._checkDistanceBetween = function (object1, object2) {
             if (Math.sqrt(Math.pow((object1.x - object2.x), 2) + Math.pow((object1.y - object2.y), 2)) <= 80) {
                 // health
                 this._health--;
-                this._healthLabel.text = this._health.toString();
-                this._healthBar.graphics.clear();
-                if (this._health >= 70) {
-                    this._healthBar.graphics.beginFill("#0f5").drawRect(50, 15, this._health, 4);
-                }
-                else if (this._health >= 40 && this._health < 70) {
-                    this._healthBar.graphics.beginFill("#ff0").drawRect(50, 15, this._health, 4);
-                }
-                else {
-                    this._healthBar.graphics.beginFill("#f00").drawRect(50, 15, this._health, 4);
-                }
-                this._nowTime = createjs.Ticker.getTicks();
-                console.log("Now:" + this._nowTime);
+                this._updateBirdHealth();
+                this._showHurtEffect();
                 if (this._health <= 0) {
-                    this._health = 0;
-                    this._healthLabel.text = this._health.toString();
-                    this._bird.visible = false;
-                    this.addChild(this._gameOverLabel);
-                    this._isGameOver = true;
-                }
-                // add once feather droping out but not too much
-                if (this._nowTime - this._oldTime >= 30) {
-                    this._redFeathers.push(new objects.Feather(this._bird.x, this._bird.y));
-                    this.addChild(this._redFeathers[this._redFeathers.length - 1]);
-                    this._oldTime = this._nowTime;
+                    this._setGameOver();
                 }
             }
         };
--- GreedyBird/Scripts/states/game.ts
+++ GreedyBird/Scripts/states/game.ts
@@ -35,7 +35,6 @@
         public start(): void {
             // variables
             this._oldTime = createjs.Ticker.getTicks();
-            console.log("Old: " + this._oldTime);
             // add background
             
             this._background = new objects.Background();
@@ -63,8 +62,6 @@
             this.addChild(this._healthLabel);
 
 
-            this._gameOverLabel = new objects.Label("Game Over", "60px Consolas", "#0ff", 320, 240);
-
             // add bird
             this._bird = new objects.Bird();            
             this.addChild(this._bird);           
@@ -74,28 +71,60 @@
                 this._dragons[index] = new objects.Dragon();
                 this.addChild(this._dragons[index]);
             }
+
+
+            // When Game Over +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+            this._gameOverLabel = new objects.Label("Game Over", "60px Consolas", "#0ff", 320, 140);
+            // playAgainButton
+            this._playAgainButton = new objects.Button("playAgainButton", 320, 240);
+            this._playAgainButton.on("click", this._clickPlayAgainButton, this);
+            this._playAgainButton.visible = false;
+            this.addChild(this._playAgainButton);
+
             
             stage.addChild(this);
         } // end of start()
 
+        //
+        private _clickPlayAgainButton(event: createjs.MouseEvent): void {
+            this._resetGame();
+        }
+
+        private _resetGame(): void {
+            // reset
+            this._health = 100;
+            this._isGameOver = false;
+            this._gameOverLabel.visible = false;
+            this._healthLabel.text = this._health.toString();
+            this._healthBar.graphics.beginFill("#0f5").drawRect(50, 15, this._health, 4);
+            this._bird.visible = true;
+            this._bird.y = 150;
+            this._playAgainButton.visible = false;
+        }
 
         public update(): void {
             this._bird.update();
             this._background.update();
             
+            this._updateDragons_CheckCollision();
+            this._updateFeathers_RemoveFeathers();
 
+
+        }// end of update
+
+        private _updateDragons_CheckCollision(): void{
+            // because simply reset the positions of dragons, number of dragons never change
             for (var index = 0; index < 3; index++) {
-                this._dragons[index].update(); 
+                this._dragons[index].update();
                 if (!this._isGameOver) {
-                    this._checkDistanceBetween(this._bird, this._dragons[index]);  
-                }                           
+                    this._checkDistanceBetween(this._bird, this._dragons[index]);
+                }
             }
+        }
 
-            //
-            // update & remove feathers
-            
+        private _updateFeathers_RemoveFeathers(): void {
             if (this._redFeathers.length > 0) {
-                console.log("Feathers: " + this._redFeathers.length);
+                //console.log("Feathers: " + this._redFeathers.length);
                 for (var i = 0, len = this._redFeathers.length; i < len; i++) {
                     this._redFeathers[i].update();
                     if (this._redFeathers[i].y > 680) {
@@ -105,43 +134,52 @@
                     }
                 }
             }
+        }
 
-        }// end of update
+        private _updateBirdHealth(): void {
 
+            this._healthLabel.text = this._health > 0 ? this._health.toString() : "0";
+
+            this._healthBar.graphics.clear();
+            if (this._health >= 70) {
+                this._healthBar.graphics.beginFill("#0f5").drawRect(50, 15, this._health, 4);
+            } else if (this._health >= 40 && this._health < 70) {
+                this._healthBar.graphics.beginFill("#ff0").drawRect(50, 15, this._health, 4);
+            } else {
+                this._healthBar.graphics.beginFill("#f00").drawRect(50, 15, this._health, 4);
+            }
+        }
+
+        private _setGameOver(): void {
+
+            this._bird.visible = false;
+            this.addChild(this._gameOverLabel);
+            this._isGameOver = true;
+
+            this._playAgainButton.visible = true;
+        }
+
+        private _showHurtEffect(): void {
+            // add once feather droping out but not too much
+            this._nowTime = createjs.Ticker.getTicks();
+            if (this._nowTime - this._oldTime >= 30) {
+                this._redFeathers.push(new objects.Feather(this._bird.x, this._bird.y));
+                this.addChild(this._redFeathers[this._redFeathers.length - 1]);
+                this._oldTime = this._nowTime;
+            }
+        }
 
         private _checkDistanceBetween(object1: objects.Bird, object2: createjs.Sprite): void {
+
             if (Math.sqrt(Math.pow((object1.x - object2.x), 2) + Math.pow((object1.y - object2.y), 2)) <= 80){
 
                 // health
                 this._health--;
-                this._healthLabel.text = this._health.toString();
-                this._healthBar.graphics.clear();
-                if (this._health >= 70) {
-                    this._healthBar.graphics.beginFill("#0f5").drawRect(50, 15, this._health, 4);
-                } else if (this._health >= 40 && this._health < 70) {
-                    this._healthBar.graphics.beginFill("#ff0").drawRect(50, 15, this._health, 4);
-                } else {
-                    this._healthBar.graphics.beginFill("#f00").drawRect(50, 15, this._health, 4);
-                }
-
-
-                this._nowTime = createjs.Ticker.getTicks();
-                console.log("Now:" + this._nowTime);
+                this._updateBirdHealth();      
+                this._showHurtEffect();                
 
                 if (this._health <= 0) {
-
-                    this._health = 0;
-                    this._healthLabel.text = this._health.toString();
-                    this._bird.visible = false;
-                    this.addChild(this._gameOverLabel);
-                    this._isGameOver = true;
-                }
-
-                // add once feather droping out but not too much
-                if (this._nowTime - this._oldTime >= 30) {
-                    this._redFeathers.push(new objects.Feather(this._bird.x, this._bird.y));
-                    this.addChild(this._redFeathers[this._redFeathers.length - 1]); 
-                    this._oldTime = this._nowTime;                   
+                    this._setGameOver();
                 }
                
             }
